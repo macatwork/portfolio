6fbccbded1acad9b6bf2ad1f0ba43d1ac2331595
macatwork <61879790+macatwork@users.noreply.github.com>
2020-03-13 15:13:19 +0000

    Formatted Code

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-		//Javascript to toggle the menu
-		document.getElementById('nav-toggle').onclick = function(){
-			document.getElementById("nav-content").classList.toggle("hidden");
-		}
+//Javascript to toggle the menu
+document.getElementById("nav-toggle").onclick = function() {
+  document.getElementById("nav-content").classList.toggle("hidden");
+};
