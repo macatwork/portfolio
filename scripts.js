1d4cddd0b042d80a5dd111643ff0a860b89757c3
macatwork <61879790+macatwork@users.noreply.github.com>
2020-03-16 11:32:12 +0000

    Up to date
    Need to add Modals, Card flips and Fix iPhone Screen

---
+++ scripts.js
@@ -0,0 +1,33 @@
+//Javascript to toggle the menu
+document.getElementById("nav-toggle").onclick = function() {
+  document.getElementById("nav-content").classList.toggle("hidden");
+};
+
+// Scroll Bar Progress
+const scrollLine = document.querySelector('.scroll-line');
+
+function fillScrollLine() {
+  const windowHeight    = window.innerHeight;
+  const fullHeight      = document.body.clientHeight;
+  const scrolled        = window.scrollY;
+  const percentScrolled = (scrolled / (fullHeight - windowHeight)) * 100;
+
+  scrollLine.style.width = `${percentScrolled}%`;
+}
+
+window.addEventListener('scroll', debounce(fillScrollLine));
+
+function debounce(func, wait = 15, immediate) {
+  var timeout;
+  return function() {
+    var context = this, args = arguments;
+    var later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
